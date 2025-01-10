be3859b08741d9f9141a95cce4aba06a3b2c7d00
Ilohama <leeloo.franchi@proton.me>
2025-01-10 16:41:31 +0100

    More tweaks

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import createMiddleware from "next-intl/middleware";
+
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ["en", "fr"],
+  // Used when no locale matches
+  defaultLocale: "en",
+  // Don't add a locale prefix
+  localePrefix: "always",
+});
+
+export const config = {
+  matcher: [
+    "/((?!api|_next|_vercel|__session|.*\\..*).*)",
+    // Using only the below paths seems to match every path...
+    // '/',
+    // '/(en|fr|de)/:path*'
+  ],
+};
