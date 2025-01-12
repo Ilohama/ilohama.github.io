53f6e6f83d63295e7ab5580e8382e8de085e89fc
Ilohama <leeloo.franchi@proton.me>
2025-01-12 21:39:21 +0100

    Trying fixing redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import { redirect } from "next/navigation";
+"use client";
+import { useRouter } from "next/navigation";
 
 export default function RootPage() {
-  redirect("/en");
+  const router = useRouter();
+  router.push("/en");
 }
--- src/config.ts
+++
@@ -1,4 +0,0 @@
-// export const port = process.env.PORT || 3000;
-// export const host = process.env.HOST
-//   ? `https://${process.env.HOST}`
-//   : `http://localhost:${port}`;
